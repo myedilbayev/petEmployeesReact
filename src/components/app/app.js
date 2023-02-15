560a7b318c7e6acd56ecf76a4a826639bcf96e6c
Marat Yedilbayev <yedilbayev1993@yandex.kz>
2023-02-15 11:53:40 +0600

    first push

---
+++ src/components/app/app.js
@@ -0,0 +1,33 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesList from '../employees-list/employees-list';
+import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
+import './app.css';
+
+function App() {
+
+  const data = [
+    {name: "Marat Ye", salary: 800, increase: false},
+    {name: "Ardak Kh.", salary: 1000, increase: true},
+    {name: "Konstantin K.", salary: 1500, increase: false},
+  ];
+
+
+  return (
+    <div className="app">
+        <AppInfo />
+
+        <div className="search-panel">
+            <SearchPanel/>
+            <AppFilter/>
+        </div>
+        
+        <EmployeesList data={data}/>
+        <EmployeesAddForm/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,18 @@
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+
+import './employees-list.css';
+
+const EmployeesList = ({data}) => {
+
+    const element = data.map((item) => {
+        return <EmployeesListItem {...item}/>
+    })
+
+    return (
+        <ul className="app-list list-group">
+            {element}
+        </ul>
+    )
+}
+
+export default EmployeesList;
